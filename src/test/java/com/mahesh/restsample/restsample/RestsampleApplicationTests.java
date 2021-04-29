38e28b3e947508ddea76c23c635efd359e5f5971
patilmaheshm <pmahesh.mp@gmail.com>
2021-04-29 16:28:29 -0400

    inital commit

---
+++ src/test/java/com/mahesh/restsample/restsample/RestsampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahesh.restsample.restsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestsampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
